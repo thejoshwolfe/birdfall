c5fcebee803e7be38832110a906433caede00015
Gooby <34070642+jmdiamond3@users.noreply.github.com>
2020-01-25 13:12:25 -0500

    Update a.js

--- a.js
+++ a.js
@@ -31,7 +31,7 @@ var SNAKE = "s";
 var BLOCK = "b";
 var FRUIT = "f";
 
-var tileSize = 40;
+var tileSize = 34;
 var level;
 var unmoveStuff = {undoStack:[], redoStack:[], spanId:"movesSpan", undoButtonId:"unmoveButton", redoButtonId:"removeButton"};
 var uneditStuff = {undoStack:[], redoStack:[], spanId:"editsSpan", undoButtonId:"uneditButton", redoButtonId:"reeditButton"};
